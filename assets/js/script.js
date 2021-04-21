63f62cd967de96cf49be848fd825995ed3198b01
CBear <chebert222@gmail.com>
2021-04-21 14:13:17 -0700

    added current day elements to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var getWeatherInfo = function (city, lon, lat) {
                 response.json()
                     .then(function (data) {
                         console.log(data);
+
+                        // variables for the current day
                         var getDate = new Date(data.current.dt * 1000);
                         var getTemp = data.current.temp;
                         var getWind = data.current.wind_speed;
@@ -22,18 +24,27 @@ var getWeatherInfo = function (city, lon, lat) {
 
 
                         // create html elements for page
-                        var cityEl = document.createElement("h3");
-                        cityEl.textContent = "chicago" + getDate.toLocaleDateString();
+                        var pageDay = document.querySelector("#current");
+
+                        var thisDay = document.createElement("div");
+                        thisDay.setAttribute("id", "this-day");
                         
+
+                        var cityEl = document.createElement("h3");
+                        cityEl.textContent = city + " " + getDate.toLocaleDateString();
+                        
+
                         var tempEl = document.createElement("p");
                         tempEl.textContent = "Temp: " + getTemp + " °F";
+                        
 
                         var windEl = document.createElement("p");
                         windEl.textContent = "Wind: " + getWind + " mph";
+                        
 
                         var humidityEl = document.createElement("p");
                         humidityEl.textContent = "Humidity: " + getHumidity + " %";
-
+                        
                         
                         var uviEl = document.createElement("p");
                         uviEl.setAttribute("id", "uvi");
@@ -43,11 +54,17 @@ var getWeatherInfo = function (city, lon, lat) {
                         var imageEl = document.createElement("img");
                         imageEl.setAttribute("src", "http://openweathermap.org/img/wn/" + getIcon + ".png");
                         imageEl.setAttribute("alt", "open weather icon");
+                        
 
-
-                        // append elements to the page
-                        cityEl.append("#current")
-                        tempEl.append("#current")
+                        // append elements to the div
+                        thisDay.appendChild(cityEl);
+                        thisDay.appendChild(imageEl);
+                        thisDay.appendChild(tempEl);
+                        thisDay.appendChild(windEl);
+                        thisDay.appendChild(humidityEl);
+                        thisDay.appendChild(uviEl);
+                        // append the div to the page
+                        pageDay.appendChild(thisDay);
 
                     })
             }
